be504595d898771ec41cca29fbb7df7978f41503
Len1n01 <peotr2005@gmail.com>
2025-03-05 13:27:30 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,47 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
+from tkinter import *
 
-def say_hello():
-    messagebox.showinfo("Hello", "Hello World")
+def show_about():
+    tk.messagebox.showinfo("О программе ", "Опросник для университета")
+def show_help():
+    tk.messagebox.showinfo("Помощь", "Для начала опроса нажмите кнопку 'Начать опрос'")
 
 root = tk.Tk()
 root.geometry("1024x940")
-root.title("Hello World")
+root.title("Опросник")
+root.resizable(False, False)
 
-label = ttk.Label(root, text="Hello World")
-label.pack()
+# about , help
+frame_top = tk.Frame(root)
+frame_top.pack(anchor='nw')
 
-button = ttk.Button(root, text="Say Hello", command=say_hello)
-button.pack()
+btn_about = tk.Button(frame_top, text="О программе", command=show_about)
+btn_about.grid(row=0, column=0)
 
+btn_help = tk.Button(frame_top, text="Помощь", command=show_help)
+btn_help.grid(row=0, column=1)
+
+# загаловок
+title_label = ttk.Label(root, text="Опросник для университета", font=("Arial", 20, "bold"))
+title_label.pack(pady=10)
+
+# создание фрейма для кнопок
+frame_buttons = tk.Frame(root)
+frame_buttons.pack(padx=10, pady=10)
+
+# кнопка начать опрос
+# кнопка начать опрос
+btn_open = tk.Button(frame_buttons, text="Откр вопрос", font=("Arial", 14, "bold"), padx=20, pady=5, bd=2, width=20)
+btn_open.pack(pady=20, padx=10)
+
+btn_close = tk.Button(frame_buttons, text="Закры вопрос", font=("Arial", 14, "bold"), padx=20, pady=5, bd=2, width=20)
+btn_close.pack(pady=20, padx=10)
+
+btn_pazl = tk.Button(frame_buttons, text="Пазл", font=("Arial", 14, "bold"), padx=20, pady=5, bd=2, width=20)
+btn_pazl.pack(pady=20, padx=10)
+
+btn_log = tk.Button(frame_buttons, text="Логика", font=("Arial", 14, "bold"), padx=20, pady=5, bd=2, width=20)
+btn_log.pack(pady=20, padx=10)
 root.mainloop()
